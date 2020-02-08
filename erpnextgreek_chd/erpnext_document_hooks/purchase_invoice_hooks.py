ea94ce8a549500b2815c879e0d3ac54cc2e839e5
chdcomputers@gmail.com <chdcomputers@gmail.com>
2020-02-08 19:39:17 +0200

    Fix indetation issues

--- erpnextgreek_chd/erpnext_document_hooks/purchase_invoice_hooks.py
+++ erpnextgreek_chd/erpnext_document_hooks/purchase_invoice_hooks.py
@@ -3,18 +3,18 @@ from __future__ import unicode_literals
 import frappe
 
 def pi_on_submit(doc, method):
-    needsUpdate = False
+	needsUpdate = False
     
-    for item in doc.get("items"):
-        print(item.item_code)
-        pr = item.purchase_receipt
-        print(pr)
+	for item in doc.get("items"):
+		print(item.item_code)
+		pr = item.purchase_receipt
+		print(pr)
 
-    if pr is not None:
-        print("Purchase receipt found")
-        pr_doc = frappe.get_doc("Purchase Receipt", pr)
-        if pr_doc is not None:
-            print("Purchase receipt document retrieved")
+	if pr is not None:
+		print("Purchase receipt found")
+		pr_doc = frappe.get_doc("Purchase Receipt", pr)
+		if pr_doc is not None:
+			print("Purchase receipt document retrieved")
 			for pi_item in doc.get("items"):
 				for pr_item in pr_doc.get("items"):
 					print(pr_item.item_code)
@@ -23,18 +23,18 @@ def pi_on_submit(doc, method):
 							needsUpdate = True
 							pr_item.valuation_rate = pi_item.rate
 
-            if needsUpdate:
-                print("Updating")
-                # save will update landed_cost_voucher_amount and voucher_amount in PR,
-                # as those fields are allowed to edit after submit
-                pr_doc.save()
+			if needsUpdate:
+				print("Updating")
+				# save will update landed_cost_voucher_amount and voucher_amount in PR,
+				# as those fields are allowed to edit after submit
+				pr_doc.save()
 
-                # update stock & gl entries for cancelled state of PR
-                pr_doc.docstatus = 2
-                pr_doc.update_stock_ledger(allow_negative_stock=True, via_landed_cost_voucher=False)
-                pr_doc.make_gl_entries_on_cancel(repost_future_gle=False)
+				# update stock & gl entries for cancelled state of PR
+				pr_doc.docstatus = 2
+				pr_doc.update_stock_ledger(allow_negative_stock=True, via_landed_cost_voucher=False)
+				pr_doc.make_gl_entries_on_cancel(repost_future_gle=False)
 
-                # update stock & gl entries for submit state of PR
-                pr_doc.docstatus = 1
-                pr_doc.update_stock_ledger(via_landed_cost_voucher=False)
-                pr_doc.make_gl_entries()
+				# update stock & gl entries for submit state of PR
+				pr_doc.docstatus = 1
+				pr_doc.update_stock_ledger(via_landed_cost_voucher=False)
+				pr_doc.make_gl_entries()
